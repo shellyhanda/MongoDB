4890dcc8703c9703baf47b5b3c61cf48c2e5b65d
Shelly Handa <shelly.handa@gmail.com>
2019-03-24 22:28:15 -0700

    mongo

---
+++ test_db.js
@@ -0,0 +1,15 @@
+db.help()
+db.version()
+db.getName()
+show dbs
+use test
+show collections
+db.MyCollection.insert({ "mm" : 1 })
+db.movie.drop()
+db.MyCollection.find()
+use ShellyDB
+show dbs
+db.createCollection("ShellyCollection")
+db.ShellyCollection.save({"name" : 9})
+db.ShellyCollection.find().pretty()
+db.ShellyCollection.find({"name":{$ne :0}})
